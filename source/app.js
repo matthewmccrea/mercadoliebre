eed01a29fed525080b1b25105dd06731640c3831
Matthew <Matt@MacBook-Pro-4.local>
2022-06-29 18:04:37 -0300

    Primer commit

---
+++ source/app.js
@@ -0,0 +1,23 @@
+const express= require ("express");
+const app=express();
+const path= require("path");
+const port=process.env.PORT||3000
+app.listen(port, () => console.log("Servidor funcionando"))
+
+
+
+const public= path.resolve(__dirname, '../public')
+
+
+
+app.use(express.static("../public"))
+
+
+
+
+
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.html')))
+
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, './views/register.html')))
+
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, './views/login.html')))
